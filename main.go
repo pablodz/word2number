0d665ddf318e04d14eb52ba465ece16c3f565af1
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2022-10-25 00:06:54 -0500

    time

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/pablodz/word2number/word2number/lang"
 )
 
 func main() {
-	text := "Mi código es diecisiete mil setecientos treinta y dos"
+	tstart := time.Now()
+	text := "Mi código es diecisiete mil setecientos treinta y dos perros y gatos, dos perros y gatos "
 	textFixed, err := lang.Text2NumES(text)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("text:\t\t", text)
 	fmt.Println("textFixed:\t", textFixed)
+	fmt.Println("time:\t\t", time.Since(tstart))
 }
